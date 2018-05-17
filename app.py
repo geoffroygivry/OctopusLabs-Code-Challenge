90eed6fdb179afad85dd5b62035da8e63b91a931
Geoffroy Givry <geoff.givry@gmail.com>
2018-05-17 22:30:01 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+# importing Tornado module
+import tornado.ioloop
+import tornado.web
+
+# Setting up the main template
+class MainHandler(tornado.web.RequestHandler):
+  def get(self):
+    self.render('index.html')
+
+# setting up the root of the web app
+application = tornado.web.Application([
+  (r"/", MainHandler)
+])
+  
+  
+settings = {
+    'template_path': os.path.join(os.path.dirname(__file__), 'templates'),
+    'static_path': os.path.join(os.path.dirname(__file__), 'static'),
+    'debug': True
+    }
+
+# Start the server at port 8889
+if __name__ == "__main__":
+  PortNumber = str(8889)
+  print(r'Server Running at http://localhost:' + PortNumber + r'/')
+  print(r'To close press ctrl + c')
+  application.listen(PortNumber)
+  tornado.ioloop.IOLoop.instance().start()
